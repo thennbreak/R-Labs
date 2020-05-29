1b5c130bac1424257c19585277f56c4490d91453
Tyler Henn <37907509+thennbreak@users.noreply.github.com>
2020-05-29 19:01:48 -0400

    Update BootstapCI.R

--- Lab1/BootstapCI.R
+++ Lab1/BootstapCI.R
@@ -3,15 +3,23 @@ x = c(49.9996,49.9994,49.9993,
       49.9995,49.9994,49.9995, 
       49.9994)
 B = 2000
-y = sapply(1:B, function(i){mean(sample(x, size = length(x), replace = T))})
-hist(y)
+mass = sapply(1:B, function(i){mean(sample(x, size = length(x), replace = T))})
+mu = mean(mass)
+sdmass = sd(mass)
+
+#Graphical Options Start#
+h = hist(mass, col="black", border="white", xlab = "Mass in grams", main = "Methamphetamine Average Mass")
+xfit = seq(min(mass), max(mass), length = 40)
+yfit = dnorm(xfit, mean = mean(mass), sd = sd(mass))
+yfit = yfit * diff(h$mids[1:2]) * length(mass)
+lines(xfit, yfit, col="red", lwd=3)
+#Graphical Options End#
+
+
 conf = .99
 alpha = 1 - conf
-quantile(y, probs = c(alpha/2, 1 - alpha/2))#two-sided 99%
+quantile(mass, probs = c(alpha/2, 1 - alpha/2))#two-sided
 conf2 = .95
 alpha2 = 1 - conf2
-quantile (y, probs = c(alpha2/2, 1 - alpha2/2))#two-sided 95%
-
-sd(y)
-
-
+quantile (mass, probs = c(alpha2/2, 1 - alpha2/2))#two-sided
+sd(mass)
