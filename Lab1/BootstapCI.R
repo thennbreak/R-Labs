d60996994abf0669c0e6bd6fba3c639bdc91bb26
Tyler Henn <37907509+thennbreak@users.noreply.github.com>
2020-05-29 18:29:50 -0400

    Update BootstapCI.R

--- Lab1/BootstapCI.R
+++ Lab1/BootstapCI.R
@@ -7,11 +7,11 @@ y = sapply(1:B, function(i){mean(sample(x, size = length(x), replace = T))})
 hist(y)
 conf = .99
 alpha = 1 - conf
-quantile(y, probs = c(alpha/2, 1 - alpha/2))#two-sided
+quantile(y, probs = c(alpha/2, 1 - alpha/2))#two-sided 99%
 conf2 = .95
 alpha2 = 1 - conf2
-quantile (y, probs = c(alpha2/2, 1 - alpha2/2))#two-sided
+quantile (y, probs = c(alpha2/2, 1 - alpha2/2))#two-sided 95%
 
-quantile(y, probs = c(alpha))#one-sided lowerbound
-quantile(y, probs = c(1-alpha))#one-sided upperbound
+sd(y)
+
 
